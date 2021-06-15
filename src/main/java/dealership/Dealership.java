d9459530acee0463d696f6397a861bc25ddd0814
Joshua <jssmiller94@gmail.com>
2021-06-15 15:38:11 +0100

    woohoo

--- src/main/java/dealership/Dealership.java
+++ src/main/java/dealership/Dealership.java
@@ -1,4 +1,28 @@
 package dealership;
 
+import vehicles.Vehicle;
+
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private ArrayList<Vehicle> stock;
+    private int till;
+
+    public Dealership(ArrayList stock, int till){
+        this.stock = new ArrayList<>();
+        this.till = till;
+    }
+
+    public ArrayList<Vehicle> getStock() {
+        return stock;
+    }
+
+    public int getTill() {
+        return till;
+    }
+
+    public void addToStock(Vehicle vehicle){
+        this.stock.add(vehicle);
+    }
 }
